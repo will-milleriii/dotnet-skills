dc9ca4a0e94e70a0e4f55236c1291c8caf183fdb
Will <willmiller629@gmail.com>
2026-04-09 22:44:11 -0400

    unit tests for CircularBuffer

--- tests/DataStructures.Tests/DataStructuresTests.cs
+++ tests/DataStructures.Tests/DataStructuresTests.cs
@@ -155,4 +155,157 @@ public class DataStructuresTests
     }
 
     #endregion
+    
+    #region Circular Buffer
+
+    [Fact]
+    public void CircularBuffer_CanEnqueueOnSingleElement()
+    {
+        var circularBuffer = new CircularBuffer<int>(1);
+        circularBuffer.Enqueue(1);
+        
+        Assert.Equal(1, circularBuffer.Count);
+        var peek =  circularBuffer.Peek();
+        Assert.Equal(1, peek);
+    }
+
+    [Fact]
+    public void CircularBuffer_DequeueReturnsInFIFO()
+    {
+        var circularBuffer = new CircularBuffer<int>(3);
+        circularBuffer.Enqueue(1);
+        circularBuffer.Enqueue(2);
+        circularBuffer.Enqueue(3);
+        
+        var firstDequeue =  circularBuffer.Dequeue();
+        Assert.Equal(1, firstDequeue);
+        
+        var secondDequeue =  circularBuffer.Dequeue();
+        Assert.Equal(2, secondDequeue);
+        var thirdDequeue =  circularBuffer.Dequeue();
+        Assert.Equal(3, thirdDequeue);
+    }
+
+    [Fact]
+    public void CircularBuffer_IsFull()
+    {
+        var circularBuffer = new CircularBuffer<int>(3);
+        circularBuffer.Enqueue(1);
+        circularBuffer.Enqueue(2);
+        circularBuffer.Enqueue(3);
+        Assert.True(circularBuffer.IsFull);
+    }
+
+    [Fact]
+    public void CircularBuffer_IsEmpty()
+    {
+        var circularBuffer = new CircularBuffer<int>(3);
+        Assert.True(circularBuffer.IsEmpty);
+    }
+
+    [Fact]
+    public void CircularBuffer_ISEmptyAndIsFull_HandleStateChanges()
+    {
+        var circularBuffer = new CircularBuffer<int>(3);
+        Assert.False(circularBuffer.IsFull);
+        Assert.True(circularBuffer.IsEmpty);
+        
+        circularBuffer.Enqueue(1);
+        Assert.False(circularBuffer.IsFull);
+        Assert.False(circularBuffer.IsEmpty);
+        
+        circularBuffer.Enqueue(2);
+        Assert.False(circularBuffer.IsFull);
+        Assert.False(circularBuffer.IsEmpty);
+        
+        circularBuffer.Enqueue(3);
+        Assert.True(circularBuffer.IsFull);
+        Assert.False(circularBuffer.IsEmpty);
+
+        circularBuffer.Dequeue();
+        Assert.False(circularBuffer.IsEmpty);
+        Assert.False(circularBuffer.IsFull);
+        
+        circularBuffer.Dequeue();
+        Assert.False(circularBuffer.IsEmpty);
+        Assert.False(circularBuffer.IsFull);
+        
+        circularBuffer.Dequeue();
+        Assert.True(circularBuffer.IsEmpty);
+        Assert.False(circularBuffer.IsFull);
+    }
+
+    [Fact]
+    public void CircularBuffer_OverwriteHead()
+    {
+        var circularBuffer = new CircularBuffer<int>(3);
+        circularBuffer.Enqueue(1);
+        circularBuffer.Enqueue(2);
+        circularBuffer.Enqueue(3);
+        
+        var oldHead =  circularBuffer.Peek();
+        circularBuffer.Enqueue(4);
+        var newHead =  circularBuffer.Peek();
+        Assert.Equal(2, newHead);
+        Assert.False(oldHead == newHead);
+    }
+
+    [Fact]
+    public void CircularBuffer_PartiallyFilledBuffer()
+    {
+        var circularBuffer = new CircularBuffer<int>(3);
+        circularBuffer.Enqueue(1);
+        circularBuffer.Enqueue(2);
+        
+        var totalMembers = circularBuffer.Count;
+        
+        Assert.Equal(2, totalMembers);
+    }
+
+    [Fact]
+    public void CircularBuffer_Iteration_PartiallyFilledBuffer()
+    {
+        var circularBuffer = new CircularBuffer<int>(5);
+        circularBuffer.Enqueue(1);
+        circularBuffer.Enqueue(2);
+        
+        Assert.False(circularBuffer.IsFull);
+        Assert.Equal(new[] {1, 2}, circularBuffer);
+    }
+
+    [Fact]
+    public void CircularBuffer_Iteration_HandlesWrapAround()
+    {
+        var circularBuffer = new CircularBuffer<int>(3);
+        circularBuffer.Enqueue(1);
+        circularBuffer.Enqueue(2);
+        circularBuffer.Enqueue(3);
+        Assert.True(circularBuffer.IsFull);
+        
+        circularBuffer.Dequeue();
+        circularBuffer.Enqueue(4);
+        
+        Assert.Equal(new[] { 2, 3, 4}, circularBuffer);
+    }
+
+    [Fact]
+    public void CircularBuffer_DequeueOnEmptyThrows()
+    {
+        var circularBuffer = new CircularBuffer<int>(3);
+        Assert.Throws<InvalidOperationException>(() => circularBuffer.Dequeue());
+    }
+
+    [Fact]
+    public void CircularBuffer_PeekOnEmptyThrows()
+    {
+        var circularBuffer = new CircularBuffer<int>(3);
+        Assert.Throws<InvalidOperationException>(() => circularBuffer.Peek());
+    }
+
+    [Fact]
+    public void CircularBuffer_InvalidCapacityThrows()
+    {
+        Assert.Throws<ArgumentOutOfRangeException>(() => new CircularBuffer<int>(-1));
+    }
+    #endregion
 }
